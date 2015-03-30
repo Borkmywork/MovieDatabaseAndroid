b87db458f896e3c8e14381b562a2063d8172f17d
Jack <jackm@aloompa.com>
2015-03-30 16:50:31 -0500

    added findDirectorActivity

---
+++ app/src/main/java/com/jack/moviedatabaseandroid/FindDirectorActivity.java
@@ -0,0 +1,10 @@
+package com.jack.moviedatabaseandroid;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Jack on 3/30/15.
+ */
+public class FindDirectorActivity extends ActionBarActivity{
+
+}
