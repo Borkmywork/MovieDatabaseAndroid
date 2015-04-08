d638dbc75b69b1b6330fbbe5d9fa340bb61ef3fa
Jack <jackm@aloompa.com>
2015-04-08 15:32:50 -0500

    created sql test class

--- app/src/main/java/com/jack/moviedatabaseandroid/FindDirectorActivity.java
+++ app/src/main/java/com/jack/moviedatabaseandroid/FindDirectorActivity.java
@@ -12,5 +12,6 @@ public class FindDirectorActivity extends ActionBarActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.find_director);
+
     }
 }
