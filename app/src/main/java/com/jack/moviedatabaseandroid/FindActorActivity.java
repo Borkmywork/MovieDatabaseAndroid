272ab753f00c71df4c145f4b26cb6f296e39778b
Jack <jackm@aloompa.com>
2015-04-13 16:49:24 -0500

    added code to query database to findActorActivity

--- app/src/main/java/com/jack/moviedatabaseandroid/FindActorActivity.java
+++ app/src/main/java/com/jack/moviedatabaseandroid/FindActorActivity.java
@@ -1,6 +1,7 @@
 package com.jack.moviedatabaseandroid;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -14,11 +15,26 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 
 /**
  * Created by Dan on 3/30/15.
  */
 public class FindActorActivity extends Activity{
+
+    String url = "jdbc:mysql://98.130.0.90:3306/pggarla_movies";
+    String user = "pggarla_preader";
+    String pass = "Csc4610mysql";
+    Connection conn;
+    ArrayList<String> movies = new ArrayList<>();
+    ArrayAdapter<String> mAdapter;
+    ListView listActorResults;
+    String actor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,81 +43,74 @@ public class FindActorActivity extends Activity{
 
 
         //Results List
-        final ListView listActorResults = (ListView) findViewById(R.id.listActorResults);
-
-        //Example list of movies
-        String[] movies = new String[]{
-                "Movie 1",
-                "Movie 2",
-                "Movie 3",
-                "Movie 4",
-                "Movie 5",
-                "Movie 6",
-                "Movie 7",
-                "Movie 8",
-                "Movie 9",
-                "Movie 10"
-        };
+        listActorResults = (ListView) findViewById(R.id.listActorResults);
 
         // Define a new Adapter
         // First parameter - Context
         // Second parameter - Layout for the row
         // Third parameter - ID of the TextView to which the data is written
         // Forth - the Array of data
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
                 android.R.id.text1, movies);
 
         //Assign adapter to ListView
-        listActorResults.setAdapter(adapter);
+        listActorResults.setAdapter(mAdapter);
 
 
         //ListView item click listener
-        listActorResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //Get item value
-                String itemValue = (String) listActorResults.getItemAtPosition(position);
-
-                //Toast
-                Toast.makeText(getApplicationContext(), itemValue, Toast.LENGTH_SHORT).show();
-            }
-        });
+//        listActorResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                //Get item value
+//                String itemValue = (String) listActorResults.getItemAtPosition(position);
+//
+//                //Toast
+//                Toast.makeText(getApplicationContext(), itemValue, Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
         //Search button
-        final Button btnfindactor = (Button) findViewById(R.id.btnfindactor);
+        Button btnfindactor = (Button) findViewById(R.id.btnfindactor);
+        final EditText editTxtActorName = (EditText) findViewById(R.id.editTxtActorName);
+
 
         btnfindactor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //When the search button is pressed
+                actor = editTxtActorName.getText().toString();
+                excecuteQuery();
                 listActorResults.setVisibility(View.VISIBLE);    //Show ListView
             }
         });
 
         //Text Changed Listener
-        final EditText editTxtActorName = (EditText) findViewById(R.id.editTxtActorName);
-        editTxtActorName.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//        editTxtActorName.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                if (!editTxtActorName.getText().toString().isEmpty())
+//                    btnfindactor.setEnabled(true);     //Enable button if EditText isn't empty
+//                else {
+//                    btnfindactor.setEnabled(false);    //Disable button if EditText is empty
+//                    listActorResults.setVisibility(View.INVISIBLE);      //Hide ListView
+//                }
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//
+//            }
+//        });
 
-            }
+    }
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (!editTxtActorName.getText().toString().isEmpty())
-                    btnfindactor.setEnabled(true);     //Enable button if EditText isn't empty
-                else {
-                    btnfindactor.setEnabled(false);    //Disable button if EditText is empty
-                    listActorResults.setVisibility(View.INVISIBLE);      //Hide ListView
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
+    public void excecuteQuery() {
+        new ActorDBConnect().execute("");
     }
 
     @Override
@@ -125,5 +134,54 @@ public class FindActorActivity extends Activity{
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    private class ActorDBConnect extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... params) {
+            String entry = "";
+            try {
+
+                Class.forName("com.mysql.jdbc.Driver").newInstance();
+                conn = DriverManager.getConnection(url, user, pass);
+
+            } catch (java.sql.SQLException e1) {
+                e1.printStackTrace();
+            } catch (ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+                e.printStackTrace();
+            }
+            try {
+
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery("SELECT distinct title.title FROM cast_info\n" +
+                        "INNER JOIN name ON name.id=person_id\n" +
+                        "INNER JOIN title on title.id=cast_info.movie_id\n" +
+                        "WHERE title.kind_id = 1 AND name.name LIKE 'Wayne, John' AND cast_info.role_id = 1\n" +
+                        "ORDER BY title.title");
+                while (rs.next()) {
+                   movies.add(rs.getString(1));
+                }
+                rs.close();
+                stmt.close();
+                conn.close();
+                System.out.println(entry);
+            } catch (java.sql.SQLException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+
+
+
+
 
 }
